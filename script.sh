fc37d62208665b8f1ded179bd758b7e72e54b7e3
Malak Zia <malakzia@live.com>
2017-01-02 08:55:49 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,30 +1,24 @@
-#!/bin/bash
-
 sudo su
 printf '\033[0;31m'
 echo 'Install PHP and Apache'
 sudo apt-get -y install php7.0 libapache2-mod-php7.0 mcrypt php7.0-mcrypt php-mbstring php-pear php7.0-dev apache2
 
-echo 'Install PHP Required PHP Extensions'
+# 'Install PHP Required PHP Extensions'
 sudo phpenmod mcrypt
 sudo phpenmod mbstring
 sudo a2enmod rewrite
 sudo systemctl restart apache2
 
-echo 'Install UnixODBC UTF16 Package'
 
+
+
+# 'Import Microsoft Repository'
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-tools.list
-
-sudo apt-get update
-sudo apt-get install unixodbc-dev-utf16
-
-echo 'Import Microsoft Repository'
-
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
 
 
-echo 'Install PDO Sql Drivers'
+# 'Install PDO Sql Drivers'
 sudo apt-get update
 sudo ACCEPT_EULA=Y apt-get install msodbcsql
 sudo apt-get install unixodbc-dev-utf16
@@ -34,26 +28,21 @@ echo "extension=/usr/lib/php/20151012/pdo_sqlsrv.so" >> /etc/php/7.0/apache2/php
 echo "extension=/usr/lib/php/20151012/sqlsrv.so" >> /etc/php/7.0/cli/php.ini
 echo "extension=/usr/lib/php/20151012/pdo_sqlsrv.so" >> /etc/php/7.0/cli/php.ini
 
-
-
-
-
-
-
-
+# 'Install Composer'
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 
+# 'Create Laravel Project'
 cd /var/www/html/
 composer create-project laravel/laravel laravel --prefer-dist
 
 cd laravel
 
-
+# 'Modify Apache Virtual Host'
 echo $'<VirtualHost *:80>\nDocumentRoot /var/www/html/laravel/public\n<Directory /var/www/html/laravel/public>\nAllowOverride All\nRequire all granted\n</Directory>\n</VirtualHost>' > /etc/apache2/sites-available/000-default.conf
 sudo chmod -R 777 /var/www/html/laravel
 
-
+# 'Restart Apache'
 systemctl restart apache2
 
 
