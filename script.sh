79a4638955ce23acfe1520ae3e4fddd3ea885eaa
Malak Zia <malakzia@live.com>
2017-01-02 07:27:56 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
+sudo su
 printf '\033[0;31m'
-
+echo 'Install PHP and Apache'
 sudo apt-get -y install php7.0 libapache2-mod-php7.0 mcrypt php7.0-mcrypt php-mbstring php-pear php7.0-dev apache2
 
-sudo su
+echo 'Install PHP Required PHP Extensions'
+sudo phpenmod mcrypt
+sudo phpenmod mbstring
+sudo a2enmod rewrite
+sudo systemctl restart apache2
+
+echo 'Install UnixODBC UTF16 Package'
+
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-tools.list
-exit
+
 sudo apt-get update
 sudo apt-get install unixodbc-dev-utf16
 
-sudo su
+echo 'Import Microsoft Repository'
+
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
 
 
+echo 'Install PDO Sql Drivers'
 sudo apt-get update
 sudo ACCEPT_EULA=Y apt-get install msodbcsql
 sudo apt-get install unixodbc-dev-utf16
@@ -24,15 +34,12 @@ echo "extension=/usr/lib/php/20151012/pdo_sqlsrv.so" >> /etc/php/7.0/apache2/php
 echo "extension=/usr/lib/php/20151012/sqlsrv.so" >> /etc/php/7.0/cli/php.ini
 echo "extension=/usr/lib/php/20151012/pdo_sqlsrv.so" >> /etc/php/7.0/cli/php.ini
 
-sudo phpenmod mcrypt
-sudo phpenmod mbstring
-sudo a2enmod rewrite
-sudo systemctl restart apache2
-
-exit
 
 
-sudo su
+
+
+
+
 
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
@@ -49,7 +56,8 @@ sudo chmod -R 777 /var/www/html/laravel
 
 systemctl restart apache2
 
-exit
+
 
 printf '\033[0m'
 echo $'\n\n\nLaravel is successfully configured\nPlease configure its database configuration. See /config/database.php and .env file\n\n\n'
+exit
