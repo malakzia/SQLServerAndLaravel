d55a9d6ba51ff39fdafec5369f0b94fb961890a2
Malak Zia <malakzia@live.com>
2017-01-02 06:09:34 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,5 @@
+printf '\033[0;31m'
+
 sudo apt-get -y install php7.0 libapache2-mod-php7.0 mcrypt php7.0-mcrypt php-mbstring php-pear php7.0-dev apache2
 
 sudo su
@@ -9,7 +11,8 @@ sudo apt-get install unixodbc-dev-utf16
 sudo su
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
-exit
+
+
 sudo apt-get update
 sudo ACCEPT_EULA=Y apt-get install msodbcsql
 sudo apt-get install unixodbc-dev-utf16
@@ -19,6 +22,12 @@ echo "extension=/usr/lib/php/20151012/pdo_sqlsrv.so" >> /etc/php/7.0/apache2/php
 echo "extension=/usr/lib/php/20151012/sqlsrv.so" >> /etc/php/7.0/cli/php.ini
 echo "extension=/usr/lib/php/20151012/pdo_sqlsrv.so" >> /etc/php/7.0/cli/php.ini
 
+sudo phpenmod mcrypt
+sudo phpenmod mbstring
+sudo a2enmod rewrite
+sudo systemctl restart apache2
+
+exit
 cd /var/www/html/
 
 sudo su
@@ -28,6 +37,11 @@ cd laravel
 
 
 echo $'<VirtualHost *:80>\nDocumentRoot /var/www/html/laravel/public\n<Directory /var/www/html/laravel/public>\nAllowOverride All\nRequire all granted\n</Directory>\n</VirtualHost>' > /etc/apache2/sites-available/000-default.conf
+chown -R www-data:www-data /var/www/laravel
 
-service apache2 reload
+
+systemctl restart apache2
+
 exit
+
+printf '\033[0m'
